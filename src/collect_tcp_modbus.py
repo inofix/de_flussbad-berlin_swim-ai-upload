394312bd148457b8bcb7d92500e9597c7372d265
Michael Lustenberger <mic@hai.local>
2023-12-18 23:55:11 +0100

    TCP Modbus
    Rewrite the data part and remove hard coded stuff..

--- src/collect_tcp_modbus.py
+++ src/collect_tcp_modbus.py
@@ -1,65 +1,93 @@
 from datetime import datetime, timezone
 import gpsd
-#import geohash2
-from pymodbus.client import ModbusTcpClient
-from struct import *
+import geohash2
+import json
 import math
+import os
+from pymodbus.client import ModbusTcpClient
+from pymodbus import ModbusException
+from struct import *
 import time
-
-con_cube_ip = ''
-#gpsd.connect()
-epoch = datetime(1970,1,1,tzinfo=timezone.utc)
-now = datetime.now(timezone.utc)
-nano = int((now - epoch).total_seconds() * 1000000000)
-
-#packet = gpsd.get_current()
-#try:
-#    ghash = geohash2.encode(packet.position()[0],packet.position()[1])
-#except:
-ghash = 'u'
-
 
 def convert_bytes_to_float(int1, int2):
     hex1 = (int1).to_bytes(2,byteorder='little')
     hex2 = (int2).to_bytes(2,byteorder='little')
     return 0 if math.isnan(float(unpack("<f",hex2 + hex1)[0])) else unpack("<f",hex2 + hex1)[0]
 
-
-
-for i in range(1,9):
+def geohash():
     try:
-        client = ModbusTcpClient(con_cube_ip)
-        r = client.read_input_registers(0x0080, 116)
-        line = 'data,geohash=' + ghash + ' ' + \
-               'xP1Value=' + str(convert_bytes_to_float(r.registers[2], r.registers[3])) + \
-               ',xP2Value=' + str(convert_bytes_to_float(r.registers[10], r.registers[11])) + \
-               ',xP3Value=' + str(convert_bytes_to_float(r.registers[18], r.registers[19])) + \
-               ',xP4Value=' + str(convert_bytes_to_float(r.registers[26], r.registers[27])) + \
-               ',xP5Value=' + str(convert_bytes_to_float(r.registers[34], r.registers[35])) + \
-               ',xP6Value=' + str(convert_bytes_to_float(r.registers[42], r.registers[43])) + \
-               ',xP7Value=' + str(convert_bytes_to_float(r.registers[50], r.registers[51])) + \
-               ',xP8Value=' + str(convert_bytes_to_float(r.registers[58], r.registers[59])) + \
-               ',xP9Value=' + str(convert_bytes_to_float(r.registers[66], r.registers[67])) + \
-               ',xP10Value=' + str(convert_bytes_to_float(r.registers[74], r.registers[75])) + \
-               ',xP11Value=' + str(convert_bytes_to_float(r.registers[82], r.registers[83])) + \
-               ',xP12Value=' + str(convert_bytes_to_float(r.registers[90], r.registers[91])) + \
-               ',xP13Value=' + str(convert_bytes_to_float(r.registers[98], r.registers[99])) + \
-               ',xP14Value=' + str(convert_bytes_to_float(r.registers[106], r.registers[107])) + \
-               ',xP15Value=' + str(convert_bytes_to_float(r.registers[114], r.registers[115])) + \
-               ' ' + str(nano) + '\n'
-        break
-    except:
-        time.sleep(2)
-        continue
+        gpsd.connect()
+        packet = gpsd.get_current()
+        return geohash2.encode(packet.position()[0],packet.position()[1])
+    except ConnectionRefusedError:
+        print("Could not connect to the GPS device.")
+        raise SystemExit
+    except Exception as e:
+        print("Could not get GPS info: ", e)
+        raise SystemExit
 
+def collect_data(configfilename):
+    try:
+        with open(configfilename, "r") as f:
+            c = json.load(f)
+            d = c['data']
+            c = c['collect']
+    except FileNotFoundError:
+        print("The config file was not found at: ", configfilename)
+        raise SystemExit
+    except AttributeError as e:
+        print("The config file ('", configfilename, "') was not as expected: ", e)
+        raise SystemExit
 
+    sensor_hub_address = c['sensor_hub_address']
+    epoch = datetime(1970,1,1,tzinfo=timezone.utc)
+    now = datetime.now(timezone.utc)
+    nano = int((now - epoch).total_seconds() * 1000000000)
 
+    if c['use_gps'] == "true":
+        gh = geohash()
+    else:
+        gh = 'u'
 
-print(line)
+    client = ModbusTcpClient(sensor_hub_address)
+    # just try a couple of times if necessary...
+    for i in range(0, 15):
+        try:
+            r = client.read_input_registers(
+                    int(c['modbus_input_register_address']),
+                    int(c['modbus_input_register_count'])
+                )
+            break
+        except ModbusException:
+            time.sleep(2)
+            continue
 
+    try:
+        m = {
+                d['timestamp_name']: str(nano),
+                d['geohash_name']: gh
+            }
+        for k, v in d['value_register_map'].items():
+            try:
+                m[k] = str(convert_bytes_to_float(
+                            r.registers[int(v['modbus_reg0'])],
+                            r.registers[int(v['modbus_reg1'])]
+                        ))
+            except (KeyError, ValueError):
+                continue
+    except UnboundLocalError:
+        print("Could not connect to Modbus-TCP...")
+        raise SystemExit
+    except IndexError as e:
+        print("Have you used in the wrong register count in the config?", e)
+        raise SystemExit
 
-#with open('/home/pi/get_gps_python/new/' + str(nano), 'a') as file:
- #   file.write(line)
-
-
+    try:
+        os.makedirs(os.path.dirname(d['storage_directory_new']), exist_ok=True)
+        with open(d['storage_directory_new'] + '/' + str(nano), 'a') as f:
+            json.dump(m, f)
+    except Exception as e:
+        print("Unable to create the data file '" +
+                d['storage_directory_new'] + '/' + str(nano) + "!\n", e)
+        raise SystemExit
 
